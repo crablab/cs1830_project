3cc07e7b676ad63b0190e5b64cca3d2c097f4bba
QuackQuackBOOM <Zachary_Hughes@btinternet.com>
2018-02-28 12:58:01 +0000

    Add simple socket example

---
+++ SocketTest/00_Server.py
@@ -0,0 +1,29 @@
+import socketserver
+
+"""
+Simple example to work on
+
+May need to think bout structure of message if is slow, may be neglegable, eg.
+    send "create(id,Zombie(pos, hp, ...))",
+    send "modify(id, )"
+"""
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server.serve_forever()
