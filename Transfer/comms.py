2d69099b005a999897502d084bb2472a38ff6865
Hugh <hugh@crablab.co.uk>
2018-03-07 17:32:22 +0000

    Fix delay calculation

--- Transfer/comms.py
+++ Transfer/comms.py
@@ -142,6 +142,6 @@ def communicate(object):
         oldTime=currentTime
     while (not com.recieved.empty()):
         obj = com.recieved.get()
-        print("recieved with delay: " + str(time.time() - obj.currentTime))
+        print("recieved with delay: " + str(time.time() - json.loads(obj)['currentTime']))
         getObject(obj)
 
